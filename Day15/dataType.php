7c32190b48fabec60afdc54125e3207f5d0df65b
dirdhvataliya <137489575+dirdhvataliya@users.noreply.github.com>
2023-07-15 09:31:30 +0530

    dataType.php

---
+++ Day15/dataType.php
@@ -0,0 +1,22 @@
+<?php
+    $x = "hello world";
+    $y = "hello world";
+
+    echo $x;
+    echo "<br>";
+    echo $y;
+
+    $a = 5985;
+    var_dump($a);
+
+    $b = 10.365;
+    var_dump($b);
+
+    $cars = array("volvo","audi","jeep","mercedes","tata");
+    var_dump($cars);
+
+    $a1 = "hello world";
+    $a1 = null;
+    var_dump($a1);
+
+?>

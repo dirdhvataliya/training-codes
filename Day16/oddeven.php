8343666b8f6b34d5011ed8f3be2569cc6486f829
dirdhvataliya <137489575+dirdhvataliya@users.noreply.github.com>
2023-07-15 09:54:50 +0530

    oddeven.php

---
+++ Day16/oddeven.php
@@ -0,0 +1,17 @@
+<?php
+    $x = 2;
+    if($x%2==0){
+        echo "The number $x is even";
+    }else{
+        echo "The number $x is odd";
+    }
+    echo "<br>";
+    
+    $y = 3;
+    $z = 5;
+    if($y>$z){
+        echo "$y is greater than $z";
+    }else{
+        echo "$z is greater than $y";
+    }
+?>

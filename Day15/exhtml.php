7e45708df803bb6e6fb78ee980a8ece20ce73e71
dirdhvataliya <137489575+dirdhvataliya@users.noreply.github.com>
2023-07-15 09:49:20 +0530

    exhtml.php

---
+++ Day15/exhtml.php
@@ -0,0 +1,14 @@
+<?php
+    $single_quote = '<b><a href="https://www.google.com/">Google</a><b>';
+    echo $single_quote;
+    echo "<br>";
+    $single_quote1 = '<b><a href="https://www.php.net/">Google</a><b>';
+    echo $single_quote1;
+    echo '</br>'.'<br/>'.'<br/>';
+
+    echo <<<abc
+    <div class="a"></div>
+    <h2>hello...hii</h2>
+    <h4>how are you???</h4>
+    abc;
+?>
